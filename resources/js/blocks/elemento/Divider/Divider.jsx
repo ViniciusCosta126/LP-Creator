67db53efa23be4769d21ce4c0eddf5455ac65728
Vinicius Costa <vinicius.costa@rooster.ac>
2024-11-04 20:26:43 -0300

    Criacao componente divider

---
+++ resources/js/blocks/elemento/Divider/Divider.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+import styles from "./Divider.module.css";
+import config from "./config.json";
+import BarConfig from "./BarConfig";
+
+const Divider = () => {
+    const [settings, setSettings] = useState(config.atributos);
+    const [isOpen, setIsOpen] = useState(false);
+
+    useEffect(() => {}, [settings]);
+
+    return (
+        <>
+            <hr
+                className={styles.divider}
+                style={{
+                    backgroundColor: settings.background_color.default,
+                    marginTop: settings.margin_top.default + "px",
+                    marginBottom: settings.margin_bottom.default + "px",
+                    paddingTop: settings.padding_top.default + "px",
+                    paddingBottom: settings.padding_bottom.default + "px",
+                }}
+                onClick={() => setIsOpen(true)}
+            ></hr>
+            <BarConfig
+                setSettings={setSettings}
+                settings={settings}
+                isOpen={isOpen}
+                setIsOpen={setIsOpen}
+            />
+        </>
+    );
+};
+
+export default Divider;
